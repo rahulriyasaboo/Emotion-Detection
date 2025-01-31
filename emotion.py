a50ff070c19c199a86f3e68d12f8a3b524f0d2b1
rahulriyasaboo <138192971+rahulriyasaboo@users.noreply.github.com>
2025-01-31 12:25:02 +0530

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+face_cascade_path =r"haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
+import cv2
+from deepface import DeepFace
+
+# Load face cascade classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Path to the video file
+# video_path = r"C:\Users\91808\Downloads\videoplayback.mp4"
+#
+# # Start capturing video from the file
+# cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Break the loop when the video ends
+    if not ret:
+        break
+
+    # Convert frame to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Convert grayscale frame to RGB format
+    rgb_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2RGB)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # Extract the face ROI (Region of Interest)
+        face_roi = rgb_frame[y:y + h, x:x + w]
+
+        # Perform emotion analysis on the face ROI
+        result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
+
+        # Determine the dominant emotion
+        emotion = result[0]['dominant_emotion']
+
+        # Draw rectangle around face and label with predicted emotion
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Emotion Detection from Video', frame)
+
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and close all windows
+cap.release()
+cv2.destroyAllWindows()
